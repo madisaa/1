abfdad68897437f90a2c9890cab8f5bfa70771ad
madisaa <43490282+madisaa@users.noreply.github.com>
2018-10-25 15:07:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,22 @@ const client = new Discord.Client();
 const prefix = '*'
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Nothing,"http://twitch.tv/S-F%22)
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
+  console.log(`[Start] ${new Date()}`);
   console.log('╚[═════════════════════════════════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
   console.log('')
   console.log('Informations :')
   console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
   console.log('╚[════════════════════════════════════]╝')
   console.log('')
   console.log('╔[════════════]╗')
