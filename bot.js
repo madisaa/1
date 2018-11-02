e33a31fe28335a12b6661823b5929fc52a3acd7b
madisaa <43490282+madisaa@users.noreply.github.com>
2018-11-02 18:51:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,10 +28,14 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-client.on("ready", () => {
-  function blue() {
-    client.guilds.get('507145911609196545').roles.find("name", "new role").setColor("RANDOM");
-  };
-  setInterval(blue, 10000);
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "rr");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  }); 
 });
 client.login(process.env.BOT_TOKEN);
